ed4612876e6f323176456e3c448736e2bb686c24
Eric A. Snell <pandasys@users.noreply.github.com>
2020-05-21 15:43:17 -0400

    Update Coordinates.kt

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-val PUBLISHING_GROUP = "com.ncorti.kotlin.template"
+val PUBLISHING_GROUP = "com.ealva"
 
 object AppCoordinates {
-    const val APP_ID = "com.ncorti.kotlin.template.app"
+    const val APP_ID = "com.ealva.welite"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
