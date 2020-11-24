67161c61c385491461aa5bc634cfd375b625ccf0
Eric Snell <eric.ashley.snell@gmail.com>
2020-11-24 14:05:58 -0500

    Prepare for next development version

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -25,22 +25,22 @@ object WeLiteCoreCoordinates {
   // All parts of versioning can be up to 2 digits: 0-99
   private const val versionMajor = 0
   private const val versionMinor = 0
-  private const val versionPatch = 2
+  private const val versionPatch = 3
   private const val versionBuild = 0
 
   const val LIBRARY_VERSION_CODE = versionMajor * 1000000 + versionMinor * 10000 +
     versionPatch * 100 + versionBuild
-  const val LIBRARY_VERSION = "$versionMajor.$versionMinor.$versionPatch-$versionBuild"
+  const val LIBRARY_VERSION = "$versionMajor.$versionMinor.$versionPatch-SNAPSHOT"
 }
 
 object WeLiteJavaTimeCoordinates {
   // All parts of versioning can be up to 2 digits: 0-99
   private const val versionMajor = 0
   private const val versionMinor = 0
-  private const val versionPatch = 2
+  private const val versionPatch = 3
   private const val versionBuild = 0
 
   const val LIBRARY_VERSION_CODE = versionMajor * 1000000 + versionMinor * 10000 +
     versionPatch * 100 + versionBuild
-  const val LIBRARY_VERSION = "$versionMajor.$versionMinor.$versionPatch-$versionBuild"
+  const val LIBRARY_VERSION = "$versionMajor.$versionMinor.$versionPatch-SNAPSHOT"
 }
